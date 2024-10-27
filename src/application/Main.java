d565d2e2241ce129705826223ab97fdf17c9c1eb
AmeedDarawsheh <s12240153@stu.najah.edu>
2024-10-27 19:07:51 +0200

    v0.1.0.1

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,18 +4,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));			
-			Scene scene = new Scene(root,780,460);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			primaryStage.setTitle("IMPACT Club");
-			primaryStage.setResizable(false);			
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/application/loginController.java
+++ src/application/loginController.java
@@ -1,6 +1,8 @@
 package application;
 import java.io.IOException;
 
+import javax.swing.JTextField;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,21 +16,33 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class loginController {
-private Stage stage ;
+	private Stage stage ;
 	private Scene scene;
-	private Parent root;	
+	private Parent root;
+	@FXML
+	TextField emailTextField;
+	@FXML
+	PasswordField passwordField;
 	public void signIn(ActionEvent e) throws IOException {
 	//	System.out.println("sign in");
-		root = FXMLLoader.load(getClass().getResource("MainMenuAdmin.fxml"));
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-		stage.setTitle("Admin Setup");
-		stage.centerOnScreen();
-		stage.setResizable(false);
+		String username = emailTextField.getText();
+		String password = passwordField.getText();	
+		
+		if(true
+				/*username.equals("Admin")&&password.equals("Admin")*/
+			) {
+			root = FXMLLoader.load(getClass().getResource("MainMenuAdmin.fxml"));
+			stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+			stage.setTitle("Admin Setup");
+			stage.centerOnScreen();
+			stage.setResizable(false);
+		}	
 	}
 	public void signUp(ActionEvent e) {
-		System.out.println("sign up");	
+		System.out.println("sign up");		
+	
 	}
 }
