0b20680fd78064d5e905da9087b3d35dd2f540dc
AmeedDarawsheh <s12240153@stu.najah.edu>
2024-10-28 14:04:47 +0200

    v 0.2
    Add some icons

--- src/application/SageController.java
+++ src/application/SageController.java
@@ -1,17 +1,12 @@
 package application;
 import java.io.IOException;
-import java.lang.System.Logger;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
