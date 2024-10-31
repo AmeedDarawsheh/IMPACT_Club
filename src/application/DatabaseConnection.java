dffa0331ad844cf4daba963977a8e6962a8ce3e5
AmeedDarawsheh <s12240153@stu.najah.edu>
2024-10-31 22:55:24 +0200

    Reapply "Reapply "complete plan and link it with database""
    This reverts commit a78e1ddc3ddd3ef368ad8dde61dd75db5fce63d2.

---
+++ src/application/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection getConnection() {
+        // إعداد الاتصال بقاعدة البيانات
+        // تأكد من إعداد URL, username, و password بشكل صحيح
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "123456";
+        
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
